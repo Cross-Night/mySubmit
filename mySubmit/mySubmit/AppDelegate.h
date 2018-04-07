8a5c5496d79a657de34b599a687199cbd37d2781
demo <demo@demodeMacBook-Pro.local>
2018-04-07 16:32:35 +0800

    新建项目

---
+++ mySubmit/mySubmit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mySubmit
+//
+//  Created by demo on 2018/4/7.
+//  Copyright © 2018年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mySubmit/mySubmit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mySubmit
+//
+//  Created by demo on 2018/4/7.
+//  Copyright © 2018年 demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
